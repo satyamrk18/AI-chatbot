f910d0d404538545461aa42b46f00329fe157c9b
Satyamrk <satyamrk18@gmail.com>
2025-07-15 19:46:50 +0530

    sidebar and main chatbody structure and styling added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
-import "./App.css";
+import './App.css'
 
 function App() {
+
+
   return (
-    <div class="container">
-      <div class="background-glow"></div>
-    </div>
-  );
+<div className="container">
+  <div className="background-glow"></div>
+  {/* Your Content/Components  */}
+  
+ <div className='sidebar'></div>
+ <div className='chatbody'></div>
+
+</div>
+
+  )
 }
 
-export default App;
+export default App
