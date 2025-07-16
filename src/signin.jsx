88e405862577fd70d44b7d979ea9f9d40e5a37b1
Satyamrk <satyamrk18@gmail.com>
2025-07-17 01:18:40 +0530

    name and email save to states

--- src/signin.jsx
+++ src/signin.jsx
@@ -1,18 +1,24 @@
 import React from "react";
 import toast, { Toaster } from "react-hot-toast";
-import { UserPen,MailPlus } from "lucide-react";
+import { UserPen, MailPlus } from "lucide-react";
+import { useState, useEffect } from "react";
 import "./signin.css";
+
+
+
 const signin = () => {
+    const [name,setName] = useState("");  
+const[email,setEmail] = useState("")  
   const handlesignin = () => {
-    let customToastId = toast.custom(
+    toast.custom(
       (t) => (
         <div className="userInputs">
-            <UserPen />
-          <input type="text" placeholder="Enter Name" name="username" />
+          <UserPen />
+          <input type="text" placeholder="Enter Name" name="username"  onChange={(e)=>{setName(e.target.value)}}/>
           <MailPlus />
-          <input type="email" placeholder="Enter Email" name="email" />
+          <input type="email" placeholder="Enter Email" name="email"  onChange={(e)=>{setEmail(e.target.value)}} />
           <div className="data-btn">
-            <button className="save">Save</button>
+            <button className="save" onClick={saveData}>Save</button>
             <button className="delete">Delete</button>
           </div>
           <div
@@ -27,12 +33,16 @@ const signin = () => {
       ),
       { duration: Infinity }
     );
-  };
+    const saveData = () => {
+        alert("data added");
+    }
 
+  };
+const isSignin = "sign in"
   return (
     <div className="sign-in-container">
       <button className="sign-in-btn" onClick={handlesignin}>
-        sign in
+   sign In
       </button>
     </div>
   );
