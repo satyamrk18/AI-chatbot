8e7304cd522b7d6a60b8b6ece9778a057d042991
Satyamrk <satyamrk18@gmail.com>
2025-07-17 00:14:08 +0530

    user data input added in sign in functionality

--- src/signin.jsx
+++ src/signin.jsx
@@ -1,18 +1,35 @@
-import React from 'react'
-import toast, { Toaster } from 'react-hot-toast';
-import "./signin.css"
+import React from "react";
+import toast, { Toaster } from "react-hot-toast";
+import "./signin.css";
 const signin = () => {
-    const handlesignin = ()=>
-    {
-       toast.success("success");
-    }
-  return (
-    <div className='sign-in-container'>
-      <button className='sign-in-btn'
-      onClick={handlesignin}
-      >sign in</button>
-    </div>
-  )
-}
+    
+  const handlesignin = () => {
+    let customToastId = toast.custom(
+      (t) => (
+        <div className="userInputs">
+          <input type="text" placeholder="Enter Name" name="username" />
+          <input type="email" placeholder="Enter Email" name="email" />
+          <div className="data-btn">
+            <button className="save">Save</button>
+            <button className="delete">Delete</button>
+          </div>
+          <div className="close" onClick={()=>{toast.dismiss(t.id)}}>
+            X
+          </div>
+        </div>
+      ),
+      { duration: Infinity }
+    );
 
-export default signin
+  };
+  
+  return (
+    <div className="sign-in-container">
+      <button className="sign-in-btn" onClick={handlesignin}>
+        sign in
+      </button>
+    </div>
+  );
+};
+
+export default signin;
